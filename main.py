cbd244ae419efb3af9f9d9e53e0c87d774a4c38c
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-04-13 20:48:55 +0200

    apply gray

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import cv2
 print(cv2.__version__)
-cv2.imread("test_img.jpg")
-cv2.imshow("test", cv2.imread("test_img.jpg"))
+
+original_img = cv2.imread('test_img.jpg')
+cv2.imshow('original', original_img)
+
+gray_img = cv2.imread('test_img.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('gray', gray_img)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-
+cv2.imwrite('new_img.jpg', original_img)
