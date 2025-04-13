cf8f798a7da7081c87305b5acf4a1b22d1275793
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-04-13 21:09:34 +0200

    Apply Resizing images

--- main.py
+++ main.py
@@ -5,17 +5,10 @@ print(cv2.__version__)
 original_img = cv2.imread('test_img.jpg')
 cv2.imshow('Original', original_img)
 
-# Convert to HSV color space and display
-hsv_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2HSV)
-cv2.imshow('HSV Image', hsv_img)
-
-# Split HSV into individual channels
-H = hsv_img[:,:,0]  # Hue 
-S = hsv_img[:,:,1]  # Saturation 
-V = hsv_img[:,:,2]  # Value 
-cv2.imshow('Hue Channel', H)
-cv2.imshow('Saturation Channel', S)
-cv2.imshow('Value Channel', V)
+# Resize image to half its original dimensions
+height, width = original_img.shape[:2]
+half_img = cv2.resize(original_img, (0, 0), fx=0.5, fy=0.5)
+cv2.imshow('Half Size', half_img)
 
 # Save the original image
 cv2.imwrite('new_img.jpg', original_img)
