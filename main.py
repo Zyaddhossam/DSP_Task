bd43e7f79f6832ab7358de251d07491441db99b4
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-04-13 20:53:41 +0200

    apply BGR to Gray

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ print(cv2.__version__)
 original_img = cv2.imread('test_img.jpg')
 cv2.imshow('original', original_img)
 
-gray_img = cv2.imread('test_img.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('gray', gray_img)
+gray_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('BGR 2 Gray', gray_img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
