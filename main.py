c9f496ba356dd1b5556f9576c6cf3e0d7cf6219d
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-04-13 21:07:12 +0200

    Apply BGR to HSV

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ print(cv2.__version__)
 original_img = cv2.imread('test_img.jpg')
 cv2.imshow('Original', original_img)
 
-# Split into Blue, Green, Red channels
-B, G, R = cv2.split(original_img)
-cv2.imshow('Blue Channel', B)
-cv2.imshow('Green Channel', G)
-cv2.imshow('Red Channel', R)
+# Convert to HSV color space and display
+hsv_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2HSV)
+cv2.imshow('HSV Image', hsv_img)
 
-# Merge channels back to BGR
-merged_BGR = cv2.merge([B, G, R])
-cv2.imshow('Merged BGR', merged_BGR)
+# Split HSV into individual channels
+H = hsv_img[:,:,0]  # Hue 
+S = hsv_img[:,:,1]  # Saturation 
+V = hsv_img[:,:,2]  # Value 
+cv2.imshow('Hue Channel', H)
+cv2.imshow('Saturation Channel', S)
+cv2.imshow('Value Channel', V)
 
 # Save the original image
 cv2.imwrite('new_img.jpg', original_img)
