57a85bee0c1bf68e5fc500a53fab6de1e1fca46b
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-04-13 21:23:29 +0200

    Display using Matplotlib in a single figure

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import cv2
+import matplotlib.pyplot as plt
 print(cv2.__version__)
 
-# Read and display original image
+# Read and process images using OpenCV
 original_img = cv2.imread('test_img.jpg')
-cv2.imshow('Original Image', original_img)
-
 gray_img = cv2.imread('test_img.jpg', 0)
-cv2.imshow('Grayscale Image', gray_img)
+inv_img_arithmetic = 255 - gray_img
+inv_img_bitwise = cv2.bitwise_not(gray_img)
+# Display using Matplotlib in a single figure
+plt.figure(figsize=(12, 4))
 
-# Invert image using arithmetic operation
-inv_img1 = 255 - gray_img
-cv2.imshow('Inverted Image (Arithmetic)', inv_img1)
+plt.subplot(1, 3, 1)
+plt.imshow(gray_img, cmap='gray')
+plt.title('Original Grayscale')
+plt.axis('off')
 
-# Invert image using bitwise NOT
-inv_img2 = cv2.bitwise_not(gray_img)
-cv2.imshow('Inverted Image (Bitwise NOT)', inv_img2)
+plt.subplot(1, 3, 2)
+plt.imshow(inv_img_arithmetic, cmap='gray')
+plt.title('Inverted (Arithmetic)')
+plt.axis('off')
+
+plt.subplot(1, 3, 3)
+plt.imshow(inv_img_bitwise, cmap='gray')
+plt.title('Inverted (Bitwise)')
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
 
 # Save the original image
 cv2.imwrite('new_img.jpg', original_img)
