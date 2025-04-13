693a51d4424690e4553a79625e0e6607e5d87cd9
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-04-13 20:59:18 +0200

    Apply BGR to RGB

--- main.py
+++ main.py
@@ -5,14 +5,8 @@ print(cv2.__version__)
 original_img = cv2.imread('test_img.jpg')
 cv2.imshow('Original', original_img)
 
-# Convert to grayscale and display
-gray_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('BGR 2 Gray', gray_img)
-
-# Apply threshold to create black and white image
-_, Black_White_Img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY)
-cv2.imshow('Black_White_Img', Black_White_Img)
-
+rgb_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
+cv2.imshow('BGR 2 RGB', rgb_img)
 # Save the original image
 cv2.imwrite('new_img.jpg', original_img)
 
